8c2cb603b6459381e44e3b8d6e9845d71cbf1ff2
Estebanm1812 <69942961+Estebanm1812@users.noreply.github.com>
2021-03-10 19:15:44 -0500

    Second commit

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,33 @@
 package ui;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+	
+	String line = br.readLine();
+	String [] data = line.split(" ");
+	int [] valueData = new int[line.length()];
+	
+	String line2 = br.readLine();
+	String [] prices = line2.split(" ");
+	int [] valuePrices = new int[line2.length()];
+	
+	String line3 = br.readLine();
+	int money = Integer.parseInt(line3);
+	
+	Arrays.sort(valuePrices);
 
+	
 	}
 
 }
